03bcededed017247106f40fae856fc1894f30be0
Dang Khoa Vo <135003263+TuilaBo@users.noreply.github.com>
2025-10-20 13:03:03 +0700

    Initial commit structure

---
+++ APIGatewate/src/main/java/com/khoavdse170395/apigatewate/ApiGatewateApplication.java
@@ -0,0 +1,13 @@
+package com.khoavdse170395.apigatewate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGatewateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGatewateApplication.class, args);
+	}
+
+}
